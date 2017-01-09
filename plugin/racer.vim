racer-rust/vim-racer

commit 333fdf9cfd5e2355372310222e364438d9d2510f

consider Windows path entry separator '\'

plugin/racer.vim
@@ -18,10 +18,18 @@ let g:loaded_racer = 1
 let s:save_cpo = &cpo
 set cpo&vim
 
+let s:is_win = has('win32') || has('win64')
+
 if !exists('g:racer_cmd')
-    let s:path = escape(expand('<sfile>:p:h'), '\') . '/../target/release/'
+    let s:sep = s:is_win ? '\' : '/'
+    le s:path = join([
+            \ escape(expand('<sfile>:p:h'), '\'),
+            \ '..',
+            \ 'target',
+            \ 'release',
+            \ ], s:sep)
     if isdirectory(s:path)
-        let s:pathsep = has("win32") ? ';' : ':'
+        let s:pathsep = s:is_win ? ';' : ':'
         let $PATH .= s:pathsep . s:path
     endif
     let g:racer_cmd = 'racer'

commit b322dd01f586403f9c2bdea14d2dd00dacf52081

Fix variable assignment typo
Fix typo introduced in 333fdf9cfd5e2355372310222e364438d9d2510f.

plugin/racer.vim
@@ -22,7 +22,7 @@ let s:is_win = has('win32') || has('win64')
 
 if !exists('g:racer_cmd')
     let s:sep = s:is_win ? '\' : '/'
-    le s:path = join([
+    let s:path = join([
             \ escape(expand('<sfile>:p:h'), '\'),
             \ '..',
             \ 'target',